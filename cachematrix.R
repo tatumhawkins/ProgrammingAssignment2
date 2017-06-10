95021b4aec81d281a430d71b9d13557fb96701ee
tatumhawkins <tatumhawkins87@gmail.com>
2017-06-10 11:14:09 -0500

    Add files via upload
    My solution for Week 3 assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,87 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Two functions to cache and compute the inverse of
+#a square matrix.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCachceMatrix <- function(mat = numeric()) {
+  #Sets inv to NULL in case something else was already stored there.
+  inv <- NULL
+  
+  set <- function(y) {
+    #Used to set a new matrix to mat and clear inv to NULL.
+    mat <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() {
+    #Used to fetch the current matrix in mat.
+    mat
+  } 
+  
+  setInverse <- function(inverse){
+    #Used to set a matrix to inv. Note: Does NOT compute the inverse.
+    inv <<- inverse
+  } 
+  
+  getInverse <- function() {
+    #Used to fetch the current value of inv.
+    inv
+  }
+  
+  #Creates a list of options for makeCachceMatrix.
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+  
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #Retrieves the current value of inv for the provided matrix. 
+  inv <- x$getInverse()
+  
+  if(!is.null(inv)) {
+    #If the inv value has been preivously cached,
+    #it fetches the value and returns it. 
+    message("getting cached data")
+    return(inv)
+  }
+  
+  #Assigns the current matrix to data. 
+  data <- x$get()
+  
+  #Computes the inverse of the current matrix.
+  inv <- solve(data, ...)
+  
+  #Caches the inverse of the matrix for future use.
+  x$setInverse(inv)
+  
+  #Returns the inverse of the matrix.
+  inv
+}
+
+
+
+cacheSolve <- function(x, ...) {
+  #Retrieves the current value of inv for the provided matrix. 
+  inv <- x$getInverse()
+  
+  if(!is.null(inv)) {
+    #If the inv value has been preivously cached,
+    #it fetches the value and returns it. 
+    message("getting cached data")
+    return(inv)
+  }
+  
+  #Assigns the current matrix to data. 
+  data <- x$get()
+  
+  #Computes the inverse of the current matrix.
+  inv <- solve(data, ...)
+  
+  #Caches the inverse of the matrix for future use.
+  x$setInverse(inv)
+  
+  #Returns the inverse of the matrix.
+  inv
 }
